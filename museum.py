9392f0c10b21bc7bae6cf20be36d0990761296f1
ianbdhaan <ianbertholdhaan@gmail.com>
2023-11-08 18:27:28 +0100

    Update museum.py
    some progress in the logic

--- museum.py
+++ museum.py
@@ -2,39 +2,37 @@ import numpy as np
 pieces_number = 15
 class artwork:
     categories = {}
-    def __init__(self, name,category,artist):
-        categories = {}
-        self.name = name
-        self.category = category
-        self.artist = artist
-        if self.category in categories:
-            categories[self.category][self.name] = self.artist
+    def __init__(self, artID,category,isAIMade):
+        if self.category in artwork.categories:
+            categories[self.category][self.artID] = self.isAIMade
         else:
-            categories[self.category] = {self.name: (self.artist)}
-            
-    def isCorrectGuess(self, category,name,artist):
+            categories[self.category] = {self.artID: (self.isAIMade)}
+    @classmethod
+    def isCorrectGuess(cls, category,artID, isAIMade):
         try:
-            correct_guess = artwork.categories[category][name]
+            correct_guess = cls.categories[category][artID]
         except:
             print("you are trying to access a non existing entry in the guess")
-        if correct_guess == "AI" and artist == "AI":
-            return True
-        elif correct_guess != "AI" and artist != "AI":
-            return True
-        else:
-            return False
+        return (correct_guess == isAImade)
         
         
 class player:
     def __init__(self, name):
         self.name = name
         self.place = ""
-    def guess(self, category, artname, isrealpainter):
+        self.guesses = {}
+    def guess(self, category, artID, isrealpainter):
         #TODO implements a guess method that updates some sort of tracker
         if category != self.place: 
             #in this case we change rooms
+            #closes the door in the game
             closeDoors()
-            pass
+            #changes the current place to the new room
+            self.place = category
+            #create a new entry to store the guesses
+            self.guesses[category] = {key: None for key in artwork.categories[category].keys}
+            #registers the first guess
+            self.guesses[category][artID] = artwork.isCorrectGuess(category,artID,isAIMade)
         else:
             #in this case we check if there are more paintings left to be checked
             openDoors()
