021ba144f9a8e469c576a9e39045ec96503435f0
ianbdhaan <ianbertholdhaan@gmail.com>
2023-11-16 21:20:37 +0100

    Update museum.py
    fixed the bugs

--- museum.py
+++ museum.py
@@ -4,23 +4,24 @@ pieces_number = 15
 class Artwork:
     categories = {}
     def __init__(self, artID,category,isAIMade):
-        if self.category in artwork.categories:
-            categories[self.category][self.artID] = self.isAIMade
+        if category in Artwork.categories:
+            Artwork.categories[self.category][self.artID] = self.isAIMade
         else:
-            categories[self.category] = {self.artID: (self.isAIMade)}
+            Artwork.categories[self.category] = {self.artID: (self.isAIMade)}
     @classmethod
     def isCorrectGuess(cls, category,artID, isAIMade):
         try:
             correct_guess = cls.categories[category][artID]
         except:
             print("you are trying to access a non existing entry in the guess")
-        return (correct_guess == isAImade)
+        return (correct_guess == isAIMade)
         
 class Player:
     def __init__(self, name):
         self.name = name
         self.place = ""
         self.guesses = {}
+        print(name)
     def guess(self, category, artID, isRealPainter):
         #TODO implements a guess method that updates some sort of tracker
         action = ""
@@ -31,16 +32,16 @@ class Player:
             #changes the current place to the new room
             self.place = category
             #create a new entry to store the guesses
-            self.guesses[category] = {key: None for key in artwork.categories[category].keys()}
+            self.guesses[category] = {key: None for key in Artwork.categories[category].keys()}
             #registers the first guess
-            self.guesses[category][artID] = artwork.isCorrectGuess(category,artID,isRealPainter)
+            self.guesses[category][artID] = Artwork.isCorrectGuess(category,artID,isRealPainter)
         else:
-            self.guesses[category][artID] = artwork.isCorrectGuess(category,artID,isRealPainter)
+            self.guesses[category][artID] = Artwork.isCorrectGuess(category,artID,isRealPainter)
             #we check if there are more paintings left to be checked
             if None not in self.guesses[category].values():
                 action = f"openDoors:{self.place}"
-                updateDB()
-        sys.stdout.write(action)
+                self.updateDB()
+                print(action)
     def updateDB():
         #sends the current information in guesses in order to update the database
         pass
@@ -49,12 +50,11 @@ class Player:
         #can you finish the game-> yes/no 
         #show statistics
         pass
-    if __name__ == __main__:
-        if sys.argv[1] == "-p":
-            player = Player(sys.argv[2])
-        elif sys.argv[1] == "-g":
-            player.guess(argv[2],argv[3],argv[4])
-            
+if __name__ == '__main__':
+    if sys.argv[1] == "-p":
+        player = Player(sys.argv[2])
+    elif sys.argv[1] == "-g":
+        player.guess(sys.argv[2],sys.argv[3],sys.argv[4])
             
             
         
