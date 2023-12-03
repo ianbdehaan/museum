e4334fedadd62373990bbc0fb307d35736f4b47a
49173 <jessekroll2@gmail.com>
2023-12-03 01:39:21 +0100

    add email class

--- museum.py
+++ museum.py
@@ -52,3 +52,82 @@ class Player:
         #show statistics
         pass
    
+#logindata:
+#gmail: krolljesse0@gmail.com
+#password: SDproject123456!
+#pass: gfsnhyjgslbmrrtw
+class Statistic:
+    smtp_port = 587                 # Standard secure SMTP port
+    smtp_server = "smtp.gmail.com"  # Google SMTP Server
+
+    # Set up the email lists
+    email_from = "krolljesse0@gmail.com"
+    email_list = ["jessekroll2@gmail.com"]# possible to add more emails
+    
+    pswd = "gfsnhyjgslbmrrtw" 
+
+    subject = "Score for GuessAI"
+
+    def send_emails(email_list, playername,filename):
+
+        for person in email_list:
+
+            # Make the body of the email
+            body = f"""
+            Hey {playername},
+            here are your score attached in a file. 
+            You can open the file and see how well you performed.
+            All the best, 
+            your GuessAI team
+            """
+
+            # make a MIME object to define parts of the email
+            msg = MIMEMultipart()
+            msg['From'] = Statistic.email_from
+            msg['To'] = person
+            msg['Subject'] = Statistic.subject
+
+            # Attach the body of the message
+            msg.attach(MIMEText(body, 'plain'))
+
+            # Open the file in python as a binary
+            attachment= open(filename, 'rb')  # r for read and b for binary
+
+            # Encode as base 64
+            attachment_package = MIMEBase('application', 'octet-stream')
+            attachment_package.set_payload((attachment).read())
+            encoders.encode_base64(attachment_package)
+            attachment_package.add_header('Content-Disposition', "attachment; filename= " + filename)
+            msg.attach(attachment_package)
+
+            # Cast as string
+            text = msg.as_string()
+
+            #Connect with the server
+            #print("Connecting to server...")
+            TIE_server = smtplib.SMTP(Statistic.smtp_server, Statistic.smtp_port)
+            TIE_server.starttls()
+            TIE_server.login(Statistic.email_from, Statistic.pswd)
+            #print("Succesfully connected to server")
+            
+
+
+            # Send emails to "person" as list is iterated
+            #print(f"Sending email to: {person}...")
+            TIE_server.sendmail(Statistic.email_from, person, text)
+            #print(f"Email sent to: {person}")
+            
+
+        # Close the port
+        TIE_server.quit()
+
+
+    # Run the function
+#Statistic.send_emails(Statistic.email_list, "Leon","C:/Users/49173/Downloads/Quiz3.pdf" )
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == "-p":
+        player = Player(sys.argv[2])
+    elif sys.argv[1] == "-g":
+        player.guess(sys.argv[2],sys.argv[3],bool(sys.argv[4]))
