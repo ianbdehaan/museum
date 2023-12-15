a9ed4fb86b11cce0f882eab488928ad30c17a8fb
ianbdhaan <ianbertholdhaan@gmail.com>
2023-12-15 01:57:06 +0100

    new code to save info to files and more
    code now fully integrates with the game and has code to save and load the classes from files

--- museum.py
+++ museum.py
@@ -1,5 +1,6 @@
 import sys
 import smtplib
+from json import dump, load
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
@@ -7,12 +8,15 @@ from email import encoders
 pieces_number = 15
 
 class Artwork:
+    
     categories = {}
+    
     def __init__(self, artID,category,isAIMade):
         if category in Artwork.categories:
-            Artwork.categories[self.category][self.artID] = self.isAIMade
+            Artwork.categories[category][artID] = isAIMade
         else:
-            Artwork.categories[self.category] = {self.artID: (self.isAIMade)}
+            Artwork.categories[category] = {artID: (isAIMade)}
+            
     @classmethod
     def isCorrectGuess(cls, category,artID, isAIMade):
         try:
@@ -20,117 +24,140 @@ class Artwork:
         except:
             print("you are trying to access a non existing entry in the guess")
         return (correct_guess == isAIMade)
+    
+    @classmethod
+    def from_file(cls):
+        cls.categories = load(open("artworks.txt"))
+        
+    @classmethod
+    def save_to_file(cls):
+        dump(cls.categories, open("artworks.txt", "w"))
         
 class Player:
+    
     def __init__(self, name, email):
         self.name = name
         self.email = email
         self.place = ""
         self.guesses = {}
+
+    @classmethod
+    def from_file(cls):
+        player = cls.__new__(cls)
+        player_file = open("player_info.txt", "r")
+        firstline = player_file.readline()
+        player.name, player.email = firstline.split()
+        player.place = player_file.readline()
+        try:
+            player.guesses = load(open("player_guesses.txt"))
+        except:
+            player.guesses = {}
+        return player
+        
     def guess(self, category, artID, isRealPainter):
         #TODO implements a guess method that updates some sort of tracker
         action = ""
         if category != self.place: 
             #in this case we change rooms
             #closes the door in the game
-            action = f"closeDoors\n{self.place}"
+            action = f"closeDoors {category}"
             #changes the current place to the new room
             self.place = category
             #create a new entry to store the guesses
             self.guesses[category] = {key: None for key in Artwork.categories[category].keys()}
             #registers the first guess
             self.guesses[category][artID] = Artwork.isCorrectGuess(category,artID,isRealPainter)
-            print(action)
         else:
             self.guesses[category][artID] = Artwork.isCorrectGuess(category,artID,isRealPainter)
             #we check if there are more paintings left to be checked
             if None not in self.guesses[category].values():
-                action = f"openDoors\n{self.place}"
+                action = f"openDoors {category}"
                 self.updateDB()
-                print(action)
+        return action
+    
     def updateDB():
         #sends the current information in guesses in order to update the database
         pass
-    def ends_expedition(self):
-        #TODO implements a method that handles the end of the expedition
-        #can you finish the game-> yes/no 
-        #show statistics
-        pass
-   
-#logindata:
-#gmail: krolljesse0@gmail.com
-#password: SDproject123456!
-#pass: gfsnhyjgslbmrrtw
-class Statistic:
-    def __init__(self, name):
-        self.smtp_port= 587# Standard secure SMTP port
-        self.smtp_server = "smtp.gmail.com" # Google SMTP Server
-        # Set up the email lists
-        self.email_from = "krolljesse0@gmail.com"
-        self.email_list = ["jessekroll2@gmail.com", "leonaltfeld@gmail.com", "ianbertholdhaan@gmail.com", "agata.cieliczko3@gmail.com"]# possible to add more emails
+    
+    def save_to_file(self, player_info = False, guess_info = False):
+        if player_info:
+            message = self.name + " " + self.email + "\n" + self.place
+            file = open("player_info.txt", "w")
+            file.write(message)
+            file.close()
+        if guess_info:
+            dump(self.guesses, open("player_guesses.text","w"))
+            
+    def send_email(self, files : list):
         
-        self.pswd = "gfsnhyjgslbmrrtw" 
+        smtp_port= 587# Standard secure SMTP port
+        smtp_server = "smtp.gmail.com" # Google SMTP Server
+        # Set up the email lists
+        email_from = "krolljesse0@gmail.com"
+        
+        pswd = "gfsnhyjgslbmrrtw" 
 
-        self.subject = "Test-Score for GuessAI"
+        subject = "Test-Score for GuessAI"
 
-    def send_emails(self, email_list, playername,filename):
+        # Make the body of the email
+        body = f"""
+        Hey {self.name},
+        test
+        here are your score attached in a file. 
+        You can open the file and see how well you performed.
+        All the best, 
+        your GuessAI team
+        """
 
-        for person in email_list:
+        # make a MIME object to define parts of the email
+        msg = MIMEMultipart()
+        msg['From'] = email_from
+        msg['To'] = self.email
+        msg['Subject'] = subject
 
-            # Make the body of the email
-            body = f"""
-            Hey {playername},
-            test
-            here are your score attached in a file. 
-            You can open the file and see how well you performed.
-            All the best, 
-            your GuessAI team
-            """
+        # Attach the body of the message
+        msg.attach(MIMEText(body, 'plain'))
 
-            # make a MIME object to define parts of the email
-            msg = MIMEMultipart()
-            msg['From'] = self.email_from
-            msg['To'] = person
-            msg['Subject'] = self.subject
-
-            # Attach the body of the message
-            msg.attach(MIMEText(body, 'plain'))
-
+        for file in files:
             # Open the file in python as a binary
-            attachment= open(filename, 'rb')  # r for read and b for binary
-
+            attachment = open(file, 'rb')  # r for read and b for binary
+    
             # Encode as base 64
             attachment_package = MIMEBase('application', 'octet-stream')
             attachment_package.set_payload((attachment).read())
             encoders.encode_base64(attachment_package)
-            attachment_package.add_header('Content-Disposition', "attachment; filename= " + filename)
+            attachment_package.add_header('Content-Disposition', "attachment; filename= " + file)
             msg.attach(attachment_package)
 
-            # Cast as string
-            text = msg.as_string()
+        # Cast as string
+        text = msg.as_string()
 
-            #Connect with the server
-            #print("Connecting to server...")
-            TIE_server = smtplib.SMTP(self.smtp_server, self.smtp_port)
-            TIE_server.starttls()
-            TIE_server.login(self.email_from, self.pswd)
-            #print("Succesfully connected to server")
-            
+        #Connect with the server
+        #print("Connecting to server...")
+        TIE_server = smtplib.SMTP(smtp_server, smtp_port)
+        TIE_server.starttls()
+        TIE_server.login(email_from, pswd)
+        #print("Succesfully connected to server")
+        
 
 
-            # Send emails to "person" as list is iterated
-            #print(f"Sending email to: {person}...")
-            TIE_server.sendmail(self.email_from, person, text)
-            #print(f"Email sent to: {person}")
+        # Send emails to "person" as list is iterated
+        #print(f"Sending email to: {person}...")
+        TIE_server.sendmail(email_from, self.name, text)
+        #print(f"Email sent to: {person}")
             
 
         # Close the port
         TIE_server.quit()
 
-
         # Run the function
-    def execute(self):
-        self.send_emails(self.email_list, "Champion",r"C:\Users\49173\Desktop\Sd correct git\museum\room_1_2.png")
+            
+        
+   
+#logindata:
+#gmail: krolljesse0@gmail.com
+#password: SDproject123456!
+#pass: gfsnhyjgslbmrrtw
 
 
 # stat_instance = Statistic("Example")
@@ -139,6 +166,24 @@ class Statistic:
 if __name__ == '__main__':
     if sys.argv[1] == "-p":
         player = Player(sys.argv[2],sys.argv[3])
-        sys.stdout.write(player.name + player.email)
+        player.save_to_file(player_info = True)
+        
     elif sys.argv[1] == "-g":
-        player.guess(sys.argv[2],sys.argv[3],bool(sys.argv[4]))
+        Artwork("1331", "abstract", True)
+        Artwork.save_to_file()
+        player = Player.from_file()
+        Artwork.from_file()
+        guess_result = player.guess(sys.argv[2],sys.argv[3],bool(sys.argv[4]))
+        player.save_to_file(guess_info = True)
+        print(guess_result)
+        if guess_result != "":
+            action, room = guess_result.split()
+            sys.stdout.write(guess_result)
+            if action == "closeDoors":
+                player.room = room
+                player.save_to_file(player_info = True)
+                
+    elif sys.argv[1] == "-e":
+        player = Player.from_file()
+        Artwork.from_file()
+        player.send_email()
